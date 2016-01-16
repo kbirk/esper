af10b086351cc19719b2633aa5799fb2b8aa29cd
kbirk <birk.kevin@gmail.com>
2016-01-16 13:20:43 -0500

    Fixed bug causing incorrect detections of glsl syntax vs urls. Bug was caused by global modifier on regex in shader parser.

--- test/core/ShaderParserTest.js
+++ test/core/ShaderParserTest.js
@@ -182,5 +182,54 @@ describe('ShaderParser', function() {
             assert( !ShaderParser.isGLSL( url1 ) );
             assert( !ShaderParser.isGLSL( url3 ) );
         });
+        it('should return true if the argument string contains a "void main() {}"', function() {
+            var source0 = [
+                    'uniform highp mat4 A[10], B, C[2];',
+                    'uniform highp mat4 D;',
+                    'varying highp vec4 v;',
+                    'void main() {',
+                        '// comment',
+                        'highp mat4 m = A * D;',
+                        'v = vec4(1.0, 1.0, 1.0, 1.0);',
+                    '}'].join('\n'),
+                source1 = [
+                    'attribute highp vec3 aVertexPosition;',
+                    'attribute highp vec3 aVertexNormal;',
+                    'uniform highp mat4 uModelMatrix;',
+            		'uniform highp mat4 uViewMatrix;',
+                    'uniform highp mat4 uProjectionMatrix;',
+                    'varying highp vec3 vMVPosition;',
+                    'varying highp vec3 vMVNormal;',
+                    'void main() {',
+                        'highp vec4 mvPos = uViewMatrix * uModelMatrix * vec4( aVertexPosition, 1.0 );',
+                        'gl_Position = uProjectionMatrix * mvPos;',
+                        'vMVPosition = vec3( mvPos ) / mvPos.w;',
+                        'vMVNormal = mat3( uViewMatrix * uModelMatrix ) * aVertexNormal;',
+                    '}'].join(''),
+                source2 = [
+            		'uniform highp mat4 uViewMatrix;',
+                    'uniform highp vec3 uLightPosition;',
+                    'varying highp vec3 vMVPosition;',
+                    'varying highp vec3 vMVNormal;',
+                    'void main() {',
+                        'highp vec4 texelColor = vec4(0.8, 0.6, 0.3, 1.0);',
+                        'highp vec3 normal = normalize( vMVNormal );',
+                        'highp vec3 vLight = vec3( uViewMatrix * vec4( uLightPosition, 1.0 ) );',
+                        'highp vec3 lightDir = normalize( vLight - vMVPosition );',
+                        'highp vec3 reflectDir = reflect( -lightDir, normal );',
+                        'highp vec3 viewDir = normalize( -vMVPosition );',
+                        'highp float diffuse = max( dot( lightDir, normal ), 0.0 );',
+                        'gl_FragColor = vec4(texelColor.rgb * 0.1 + diffuse * texelColor.rgb, texelColor.a );',
+                	'}'].join(''),
+                url0 = 'shaders/vert.glsl',
+                url1 = 'shaders/main.vert',
+                url3 = 'shaders/void/main.frag';
+            assert( ShaderParser.isGLSL( source0 ) );
+            assert( ShaderParser.isGLSL( source1 ) );
+            assert( ShaderParser.isGLSL( source2 ) );
+            assert( !ShaderParser.isGLSL( url0 ) );
+            assert( !ShaderParser.isGLSL( url1 ) );
+            assert( !ShaderParser.isGLSL( url3 ) );
+        });
     });
 });
