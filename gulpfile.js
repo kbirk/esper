246cba59e0eb4ab4eaf35ec1fdc4989ccb70f1c6
kbirk <birk.kevin@gmail.com>
2016-02-27 14:53:37 -0500

    Cleaned up gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,92 +2,96 @@
 
     "use strict";
 
-    var gulp = require('gulp'),
-        runSequence = require('run-sequence'),
-        source;
+    var gulp = require('gulp');
+    var runSequence = require('run-sequence');
+    var uglify = require('gulp-uglify');
+    var buffer = require('vinyl-buffer');
+    var source = require('vinyl-source-stream');
+    var browserify = require('browserify');
+    var istanbul = require('gulp-istanbul');
+    var mocha = require('gulp-mocha');
+    var del = require('del');
+    var jshint = require('gulp-jshint');
+    var coveralls = require('gulp-coveralls');
 
-    function bundle( b, output ) {
-        source = source || require('vinyl-source-stream');
-        return b.bundle()
-            .on( 'error', function( err ) {
-                console.log( err );
-                this.emit( 'end' );
-            })
-            .pipe( source( output ) )
-            .pipe( gulp.dest( 'build' ) );
-    }
-
-    function bundleMin( b, output ) {
-        var buffer = require('vinyl-buffer'),
-            uglify = require('gulp-uglify');
-        source = source || require('vinyl-source-stream');
-        return b.bundle()
-            .on( 'error', function( err ) {
-                console.log( err );
-                this.emit( 'end' );
-            })
-            .pipe( source( output ) )
-            .pipe( buffer() )
-            .pipe( uglify() )
-            .pipe( gulp.dest( 'build' ) );
-    }
-
-    function build( root, output, minify ) {
-        var browserify = require('browserify'),
-            b = browserify( root, {
-                debug: !minify,
-                standalone: 'esper'
-            });
-        return ( minify ) ? bundleMin( b, output ) : bundle( b, output );
-    }
+    var project = 'esper';
+    var paths = {
+        root: 'src/exports.js',
+        src: 'src/**/*.js',
+        tests: 'test/**/*.js',
+        build: 'build'
+    };
 
     function handleError( err ) {
-        console.error( err.toString() );
+        console.error( err );
+        this.emit( 'end' );
+    }
+
+    function handleErrorTimeout( err ) {
+        console.error( err );
         setTimeout( function() {
             // set delay for full mocha error message
             process.exit( 1 );
         });
     }
 
+    function bundle( b, output ) {
+        return b.bundle()
+            .on( 'error', handleError )
+            .pipe( source( output ) )
+            .pipe( gulp.dest( paths.build ) );
+    }
+
+    function bundleMin( b, output ) {
+        return b.bundle()
+            .on( 'error', handleError )
+            .pipe( source( output ) )
+            .pipe( buffer() )
+            .pipe( uglify() )
+            .pipe( gulp.dest( paths.build ) );
+    }
+
+    function build( root, output, minify ) {
+        var b = browserify( root, {
+            debug: !minify,
+            standalone: project
+        });
+        return ( minify ) ? bundleMin( b, output ) : bundle( b, output );
+    }
+
     gulp.task('clean', function () {
-        var del = require('del');
         del([ 'build/*']);
     });
 
     gulp.task('lint', function() {
-        var jshint = require('gulp-jshint');
-        return gulp.src( './src/**/*.js' )
-            .pipe( jshint() )
+        return gulp.src( paths.src )
             .pipe( jshint('.jshintrc') )
             .pipe( jshint.reporter('jshint-stylish') );
     });
 
     gulp.task('test', function() {
-        var istanbul = require('gulp-istanbul'),
-            mocha = require('gulp-mocha');
-        return gulp.src( './src/**/*.js' )
-            .pipe( istanbul( { includeUntested: false } ) ) // Covering files
+        return gulp.src( paths.src )
+            .pipe( istanbul({ includeUntested: false }) ) // Covering files
             .pipe( istanbul.hookRequire() )
             .on( 'finish', function () {
-                return gulp.src( [ './test/**/*.js' ] )
-                    .pipe( mocha( { reporter: 'list' } )
-                        .on( 'error', handleError ) ) // print mocha error message
+                return gulp.src([ paths.tests ])
+                    .pipe( mocha({ reporter: 'list' })
+                        .on( 'error', handleErrorTimeout ) ) // print mocha error message
                     .pipe( istanbul.writeReports() ); // Creating the reports after tests runned
             });
     });
 
     gulp.task('coveralls', function() {
-        var coveralls = require('gulp-coveralls');
         return gulp.src('./coverage/**/lcov.info')
             .pipe( coveralls() );
     });
 
     gulp.task('build-min-js', function() {
-        return build( './src/exports.js', 'esper.min.js', true );
+        return build( paths.root, project + '.min.js', true );
     });
 
     gulp.task('build-js', function() {
-        return build( './src/exports.js', 'esper.js', false );
+        return build( paths.root, project + '.js', false );
     });
 
     gulp.task('build', function( done ) {
