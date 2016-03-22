80919be73e85dc6ffed490aed2a3a322f49f4c73
kbirk <birk.kevin@gmail.com>
2016-03-21 22:07:47 -0400

    Added StackMap and Stack / StackMap unit tests.

--- src/util/Stack.js
+++ src/util/Stack.js
@@ -2,24 +2,49 @@
 
     'use strict';
 
+    /**
+     * Instantiates a stack object.
+     * @class Stack
+     * @classdesc A stack interface.
+     */
     function Stack() {
         this.data = [];
     }
 
+    /**
+     * Push a value onto the stack.
+     *
+     * @param {*} value - The value.
+     *
+     * @returns The stack object for chaining.
+     */
     Stack.prototype.push = function( value ) {
         this.data.push( value );
         return this;
     };
 
+    /**
+     * Pop a value off the stack. Returns `undefined` if there is no value on
+     * the stack.
+     *
+     * @param {*} value - The value.
+     *
+     * @returns The value popped off the stack.
+     */
     Stack.prototype.pop = function() {
-        this.data.pop();
-        return this;
+        return this.data.pop();
     };
 
+    /**
+     * Returns the current top of the stack, without removing it. Returns
+     * `undefined` if there is no value on the stack.
+     *
+     * @returns The value at the top of the stack.
+     */
     Stack.prototype.top = function() {
         var index = this.data.length - 1;
         if ( index < 0 ) {
-            return null;
+            return;
         }
         return this.data[ index ];
     };
